5fe09ba4d20729641e0c7f741912a6e3eeecd8c9
r-m-hufford <ryan@rmhufford.com>
2021-04-18 13:29:01 -0400

    mvc built. need tests

--- src/main/java/io/zipcoder/persistenceapp/Controller/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/Controller/EmployeeController.java
@@ -1,9 +1,46 @@
 package io.zipcoder.persistenceapp.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.zipcoder.persistenceapp.Model.Employee;
+import io.zipcoder.persistenceapp.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
 
+    private EmployeeService service;
 
+    @Autowired
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    //create, read all, read one, update, delete;
+
+    @PostMapping("/employee")
+    ResponseEntity<Employee> createEmployee(@RequestBody Employee e) {
+        Employee employee = service.createEmployee(e);
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("/employee")
+    ResponseEntity<List<Employee>> readAll() {
+        List<Employee> employees = service.readAll();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/employee/{id}")
+    ResponseEntity<Employee> readOne(@PathVariable Long id) {
+        Employee employee = service.readOne(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employee/{id}")
+    ResponseEntity<Employee> update(@RequestBody Employee e, @PathVariable Long id) {
+        Employee employee = service.update(e, id);
+        return ResponseEntity.ok(employee);
+    }
 }
--- src/main/java/io/zipcoder/persistenceapp/Service/EmployeeService.java
+++ src/main/java/io/zipcoder/persistenceapp/Service/EmployeeService.java
@@ -1,7 +1,47 @@
 package io.zipcoder.persistenceapp.Service;
 
+import io.zipcoder.persistenceapp.Model.Employee;
+import io.zipcoder.persistenceapp.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    private EmployeeRepository repository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Employee createEmployee(Employee e) {
+        return repository.save(e);
+    }
+
+    public List<Employee> readAll() {
+        return repository.findAll();
+    }
+
+    public Employee readOne(Long id) {
+        return repository.findOne(id);
+    }
+
+    public Employee update(Employee e, Long id) {
+        Employee employee = readOne(id);
+        employee.setEmployeeNumber(e.getEmployeeNumber());
+        employee.setEmail(e.getEmail());
+        employee.setDepartment(e.getDepartment());
+        employee.setFirstName(e.getFirstName());
+        employee.setLastName(e.getLastName());
+        employee.setTitle(e.getTitle());
+        employee.setPhoneNumber(e.getPhoneNumber());
+        employee.setHireDate(e.getHireDate());
+        employee.setManager(e.getManager());
+
+        return employee;
+    }
 }
